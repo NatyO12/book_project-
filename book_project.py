b5021a6713899d6537299c7cc17a4eb3852e6653
NatyO12 <natyosa12@gmail.com>
2024-11-06 22:44:40 +0100

    Update book_project.py
    Le agregue unas cositas cuando puedas lo revisas porfis :)

--- book_project.py
+++ book_project.py
@@ -1,13 +1,88 @@
 import tkinter as tk
-from tkinter import messagebox, simpledialog
+from tkinter import messagebox
+import time ##Cami esto es para ordenarlo por antiguedad 
 
+##Validacion para que no tenga libros duplicados, numeros entre letras ni viceversa ##
+books = []
+def valid_book(book):
+    pattern = []
+
+    ## Que los números y las letras no este mezclados ##
+    for i, char in enumerate(book):
+        if i > 0 and char.isdigit() and book[i-1].isalpha():
+            pattern.append(True)
+        else:
+            pattern.append(False)
+    if any(pattern):
+        messagebox.showerror("ERROR", "No puede tener números entre letras, ni viceversa")
+        return False
+
+    ## Que no hayan duplicados ##
+    if book in books:
+        messagebox.showerror("ERROR", "El libro ya existe en la biblioteca")
+        return False
+    return True
+
+## Verificar que el autor contenga solo letras ##
+def valid_author(author):
+    if not author.isalpha():
+        messagebox.showerror("ERROR", "En 'autor' solo debe contener letras")
+        return False
+    return True
+
+## Verificar que el año contenga solo números ##
+def valid_year(year):
+    if not year.isdigit():
+        messagebox.showerror("ERROR", "En 'año de publicación' solo debe contener números")
+        return False
+    return True
+
+## Algoritmos de ordenamiento personalizado ##
+def quick_sort(books, start, end, sort):
+    def partition(books, start, end, sort):
+        pivot1 = books[end]
+        i = start - 1
+        for j in range(start, end):
+            if sort(books[j]) <= sort(pivot1):
+                i += 1
+                books[i], books[j] = books[j], books[i]
+        books[i + 1], books[end] = books[end], books[i + 1]
+        return i + 1
+
+    if start < end:
+        pivot = partition(books, start, end, sort)
+        quick_sort(books, start, pivot - 1, sort)
+        quick_sort(books, pivot + 1, end, sort)
+    return books
+
+def open_quick(self, books, sort):
+    return self.quick_sort(books, 0, len(books) - 1, sort)
+
+## Ordenar por: Año, Autor, titulo, Antiguedad, recomendado ##
+def sort_year(book):
+    return book.year
+def sort_author(book):
+    return book.author
+def sort_title(book):
+    return book.title
+def sort_oldest(book):
+    return book.sort_oldest
+def sort_recommended(book):
+    return book.recommended
+
+## Objeto libro ##
 class Book:
-    def __init__(self, title, author, year):
+    def __init__(self, title, author, year, recommended = False):
         self.title = title
         self.author = author
         self.year = year
+        self.recommended = recommended
+        self.arrival_date = time.time() ## Cami esto es para lo de la antiguedad
 
-class Node:
+## Listas Enlazadas ##
+
+## Lista Simple ##
+class SimpleNode:
     def __init__(self, book):
         self.book = book
         self.next = None
@@ -17,7 +92,7 @@ class SimpleList:
         self.head = None
 
     def add_book(self, book):
-        new_node = Node(book)
+        new_node = SimpleNode(book)
         if not self.head:
             self.head = new_node
         else:
@@ -56,6 +131,7 @@ class SimpleList:
             current = current.next
         return books
 
+## Lista Doble ##
 class DoublyNode:
     def __init__(self, book):
         self.book = book
@@ -126,6 +202,7 @@ class DoublyList:
             current = current.next
         return books
 
+## Lista Circular ##
 class CircularNode:
     def __init__(self, book):
         self.book = book
@@ -158,3 +235,83 @@ class CircularList:
             if current == self.head:
                 break
         return None
+
+    def delete_book(self, title):
+        current = self.head
+        prev = None
+
+        if current.book.title == title:
+            if current.next == self.head:
+                self.head = None
+            else:
+                while current.next != self.head:
+                    current = current.next
+                current.next = self.head.next
+                self.head = self.head.next
+            return True
+
+    def show_books(self):
+        current = self.head
+        books = []
+        while True:
+            books.append(current.book.title)
+            current = current.next
+            if current == self.head:
+                break
+        return books
+
+## Funcionamiento de la Biblioteca ##
+class Library:
+    def __init__(self):
+        self.simplelist = SimpleList()
+        self.doublylist = DoublyList()
+        self.circularlist = CircularList()
+
+    def add_book(self, title, author, year, recommended = False):
+        if valid_author(author) and valid_year(year) and valid_book(title):
+            new_book = Book(title, author, year, recommended)
+            self.simplelist.add_book(new_book)
+            self.doublylist.add_book(new_book)
+            if recommended:
+                self.circularlist.add_book(new_book)
+            Library.books.append(title)
+
+    def search_book(self, title):
+        book = self.simplelist.search_book(title) or self.doublylist.search_book(title) or self.circularlist.search_book(title)
+        return book
+
+    def delete_book(self, title):
+        if title in Library:
+            self.simplelist.delete_book(title)
+            self.doublylist.delete_book(title)
+            self.circularlist.delete_book(title)
+            Library.remove(title)
+        else:
+            messagebox.showerror("ERROR", "El libro no existe en la biblioteca")
+
+    def show_book(self, linked_lists = "todas", bibliographic = "bibliografía"):
+
+        if bibliographic == "year":
+            sort_b = sort_year
+        elif bibliographic == "author":
+            sort_b = sort_author
+        elif bibliographic == "title":
+            sort_b = sort_title
+        elif bibliographic == "oldest":
+            sort_b = sort_oldest
+        elif bibliographic == "recommended":
+            sort_b = sort_recommended
+        else:
+            messagebox.showerror("ERROR", "Criterio de ordenación no válido.")
+            return
+        books = self.simplelist.show_books() + self.doublylist.show_books() + self.circularlist.show_books()
+        books_sorted = quick_sort(books, 0, len(books) - 1, sort_b)
+
+        if linked_lists == "Simple":
+            return self.simplelist.show_books()
+        elif linked_lists == "Doble":
+            return self.doublylist.show_books()
+        elif linked_lists == "Circular":
+            return self.circularlist.show_books()
+        else: 
+            books_sorted
